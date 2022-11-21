c8e80afc154c088dc96bacacb78d7251a22a66a6
pablomendezroyo <mendez4a@gmail.com>
2022-11-21 13:17:34 +0100

    set minimum dappnode version

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -7,8 +7,8 @@ else
 fi
 
 case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_GNOSIS in
-"nethermind-gnosis.dappnode.eth")
-  HTTP_ENGINE="http://nethermind-gnosis.dappnode:8551"
+"nethermind-xdai.dnp.dappnode.eth")
+  HTTP_ENGINE="http://nethermind-xdai.dappnode:8551"
   ;;
 "erigon-gnosis.dnp.dappnode.eth")
   HTTP_ENGINE="http://erigon-gnosis.dappnode:8551"
